edc6b2e6cd2cbeef3a7ac9b54f71e876de7b0095
163ui <i163ui@163.com>
2018-12-01 23:06:33 +0800

    Update test.py
    failed open https://egame.qq.com/58815

--- tests/test.py
+++ tests/test.py
@@ -7,27 +7,15 @@ from you_get.extractors import (
     magisto,
     youtube,
     bilibili,
+    qq,
 )
 
 
 class YouGetTests(unittest.TestCase):
-    def test_imgur(self):
-        imgur.download('http://imgur.com/WVLk5nD', info_only=True)
-        imgur.download('http://imgur.com/gallery/WVLk5nD', info_only=True)
 
-    def test_magisto(self):
-        magisto.download(
-            'http://www.magisto.com/album/video/f3x9AAQORAkfDnIFDA',
-            info_only=True
-        )
-
-    def test_youtube(self):
-        youtube.download(
-            'http://www.youtube.com/watch?v=pzKerr0JIPA', info_only=True
-        )
-        youtube.download('http://youtu.be/pzKerr0JIPA', info_only=True)
-        youtube.download(
-            'http://www.youtube.com/attribution_link?u=/watch?v%3DldAKIzq7bvs%26feature%3Dshare',  # noqa
+    def test_egame.qq(self):
+        qq.download(
+            'https://egame.qq.com/58815',
             info_only=True
         )
 
